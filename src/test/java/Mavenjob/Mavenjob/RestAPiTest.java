02d096857b1bf9ba08aac9f5a42c877d33ea287c
Pratish <pratishpednekar11@gmail.com>
2020-12-25 13:15:24 +0530

    MavenJob files Committed

---
+++ src/test/java/Mavenjob/Mavenjob/RestAPiTest.java
@@ -0,0 +1,18 @@
+package Mavenjob.Mavenjob;
+
+import org.testng.annotations.Test;
+
+public class RestAPiTest {
+
+	@Test
+	public void postJira(){
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter(){
+		System.out.println("deleteTwitter");
+	}
+	
+	
+}
